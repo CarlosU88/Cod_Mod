4057595f93af84790485b36d23dc20e9896d3c7a
CarlosU88 <116239549+CarlosU88@users.noreply.github.com>
2022-12-02 23:50:18 -0600

    Add files via upload

--- EpidemicMod_ruido_2.m
+++ EpidemicMod_ruido_2.m
@@ -1,12 +1,12 @@
 A=[0.95 0.04 0 0;0.05 0.85 0 0; 0 0.10 1 0; 0 0.01 0 1];
 Xo=[1 0 0 0];Error=0.01;
-N=1;
+N=400;
 for k=1:N
 	X = [X [A*X(:,k)+Error*randn(4,1)]];
 end
 Y = X(:,2:(N+1));
 X = X(:,1:N);
-A1=minimo(X,Y);
+A1=minimo(X,Y)
 display(["||ArX-Y||_F = ",num2str( norm(A1*Xo'-Y,"fro"))]);
 display(["||Ar-A||_F = ",num2str( norm(A1-A,"fro"))]);
 X0 = X1 = Xo';
@@ -23,16 +23,3 @@ X0 = X1 = Xo';
   plot(X1.');
   title('Órbitas del modelo identificado')
   
-function S1=minimo(X,Y)
-D=X*transpose(X);
-I=eye(4);
-R=ones(20,1);M=zeros(20);
-for i=1:4
-  R(4*(i-1)+1:4*i,1)= 2*X(i,:)*Y';
-  M(4*(i-1)+1:4*i,4*(i-1)+1:4*i)=2*D;
-  M(17:20,4*(i-1)+1:4*i)=I;
-  M(4*(i-1)+1:4*i,17:20)=I;
-endfor
-S=pinv(M,1e-10)*R;
-S1=reshape(S(1:16),4,4);
-endfunction
